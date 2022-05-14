80f4876066fb122eba63c2993d7761eeac960b65
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-05-14 10:35:29 -0600

    Started pages for search results and brewery details.

--- main.py
+++ main.py
@@ -3,11 +3,26 @@
 # A website based on data from https://www.openbrewerydb.org/?ref=public-apis
 
 from flask import Flask, redirect, render_template, request, url_for
+import requests
+import os
 
 # Constants
-SEARCH_BY_NAME = "https://api.openbrewerydb.org/breweries?by_name="
-SEARCH_BY_CITY = "https://api.openbrewerydb.org/breweries?by_city="
-SEARCH_BY_STATE = "https://api.openbrewerydb.org/breweries?by_state="
+BREWERY_SEARCH = "https://api.openbrewerydb.org/breweries"
+API = os.environ.get("API")
+
+# Globals
+brewery_data = []
+
+
+def get_results(search, parameter):
+    global brewery_data
+    search = search.replace(" ", "_").lower()
+    response = requests.get(BREWERY_SEARCH, params={"page": 0, f"{parameter}": search})
+    response.raise_for_status()
+    brewery_data = response.json()
+    print(brewery_data[0])
+    return brewery_data
+
 
 app = Flask(__name__)
 
@@ -17,26 +32,30 @@ def home():
     if request.method == "POST":
         if request.form.get("name_search"):
             name_search = request.form.get("name_search")
-            print("NAME SEARCH =", name_search)
+            brewery_data = get_results(name_search, "by_name")
         elif request.form.get("city_search"):
             city_search = request.form.get("city_search")
-            print("CITY SEARCH =", city_search)
+            brewery_data = get_results(city_search, "by_city")
         elif (request.form.get("state_search") != "Select a State") and (
             request.form.get("state_search") != None
         ):
             state_search = request.form.get("state_search")
-            print("STATE SEARCH =", state_search)
+            brewery_data = get_results(state_search, "by_state")
+        return render_template("searchresults.html", brewery_data=brewery_data)
     return render_template("index.html")
 
 
-@app.route("/searchresults.html")
+@app.route("/searchresults")
 def search_results():
-    pass
+    return render_template("searchresults.html")
 
 
-@app.route("/brewerydetails.html")
-def brewery_details():
-    pass
+@app.route("/brewerydetails/<int:brewery_id>")
+def brewery_details(brewery_id):
+    print(brewery_data[brewery_id - 1])
+    return render_template(
+        "brewerydetails.html", brewery=brewery_data[brewery_id - 1], api=API
+    )
 
 
 if __name__ == "__main__":
